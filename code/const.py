91556653590c299f5a18a2263c589bde3585dfaf
Ygor <ygor_vasconcelos@outlook.com>
2025-08-07 21:50:27 -0300

    aula 04

--- code/const.py
+++ code/const.py
@@ -42,12 +42,32 @@ ENTITY_HEALTH = {
     'Enemy2': 60,
     'Enemy2Shot': 1,
 }
+ENTITY_DAMAGE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Player1': 1,
+    'Player1Shot': 25,
+    'Player2': 1,
+    'Player2Shot': 20,
+    'Enemy1': 1,
+    'Enemy1Shot': 20,
+    'Enemy2': 1,
+    'Enemy2Shot': 15,
+}
+
+
 ENTITY_SHOT_DELAY = {
     'Player1': 20,
     'Player2': 15,
     'Enemy1': 100,
     'Enemy2': 200,
 }
+
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P COOPERATIVE',
