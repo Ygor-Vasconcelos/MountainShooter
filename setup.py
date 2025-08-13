c84cbc94c971886526beba0b8528eca32f41f518
Ygor <ygor_vasconcelos@outlook.com>
2025-08-12 22:38:06 -0300

    Setup

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
-from tkinter.font import names
-
 from cx_Freeze import setup, Executable
-import os
-
-path = "./asset"
-asset_list = os.listdir("main.py")
-asset_list_completa = [os.path.join(path, asset).replace("\\", "/")for asset in asset_list]
-print(asset_list_completa)
 
 executables = [Executable("main.py")]
-files = {"include_files": asset_list_completa, "packages": ["pygame"]}
 
 setup(
     name="MountainShooter",
     version="1.0",
-    descripton="Mountain Shooter app",
-    options={"build.exe": files},
+    description="Mountain Shooter app",
+    options={"build_exe": {"packages": ["pygame"]}},
     executables=executables
 )
